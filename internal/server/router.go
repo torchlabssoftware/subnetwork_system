e08bb805a86d905a82602da9f1bb7ff79e479f59
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-12-18 15:13:28 +0530

    feat: add logger middleware for chi router

--- internal/server/router.go
+++ internal/server/router.go
@@ -20,6 +20,7 @@ func NewRouter(pool *sql.DB, clickHouseConn driver.Conn) http.Handler {
 	router.Use(middleware.RequestID)
 	router.Use(middleware.RealIP)
 	router.Use(middleware.Recoverer)
+	router.Use(middleware.Logger)
 	router.Use(middleware.Timeout(30 * time.Second))
 
 	router.Use(cors.Handler(cors.Options{
